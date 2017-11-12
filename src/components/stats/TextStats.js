5b89bdd14522322891a1fce89880f98c8c989646
Mateusz Kluska <kluska.mat@gmail.com>
2017-11-12 19:02:12 +0100

    Remove responsive table package and change text stats table for one from material-ui.

--- src/components/stats/TextStats.js
+++ src/components/stats/TextStats.js
@@ -1,30 +1,61 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Cell, Row, Table} from 'react-responsive-table'
+import withWidth, {SMALL} from 'material-ui/utils/withWidth'
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table'
 
 class TextStats extends React.Component {
   render() {
     const styles = {
       table: {
-        width: 'auto',
         wordBreak: 'break-all'
+      },
+      columns: {
+        reply: {
+          width: this.props.width === SMALL ? '70%' : '85%',
+          whiteSpace: 'normal'
+        },
+        count: {
+          width: this.props.width === SMALL ? '30%' : '15%',
+          textAlign: 'center',
+          whiteSpace: 'normal'
+        }
       }
     };
 
-    const {questionId, repliesToCount} = this.props;
+    const {repliesToCount} = this.props;
 
     return (
-      <Table material style={styles.table}>
-        <Row header key={questionId}>
-          <Cell key={`cell1_${questionId}`}>Reply</Cell>
-          <Cell key={`cell2_${questionId}`}>Count</Cell>
-        </Row>
-        {Object.entries(repliesToCount).map((rc, idx) =>
-          <Row striped key={idx}>
-            <Cell key={`cell1_${idx}`}>{rc[0]}</Cell>
-            <Cell key={`cell2_${idx}`}>{rc[1]}</Cell>
-          </Row>
-        )}
+      <Table
+        selectable={false}
+        style={styles.table}
+      >
+        <TableHeader
+          displaySelectAll={false}
+          adjustForCheckbox={false}
+        >
+          <TableRow>
+            <TableHeaderColumn tooltip='Reply value' style={styles.columns.reply}>
+              Reply
+            </TableHeaderColumn>
+            <TableHeaderColumn tooltip='Reply count' style={styles.columns.count}>
+              Count
+            </TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody
+          displayRowCheckbox={false}
+          showRowHover={true}
+          stripedRows={true}
+        >
+          {Object.entries(repliesToCount).map((rc, idx) => {
+            return (
+              <TableRow key={idx}>
+                <TableRowColumn style={styles.columns.reply}>{rc[0]}</TableRowColumn>
+                <TableRowColumn style={styles.columns.count}>{rc[1]}</TableRowColumn>
+              </TableRow>
+            )
+          })}
+        </TableBody>
       </Table>
     );
   }
@@ -35,4 +66,4 @@ TextStats.propTypes = {
   repliesToCount: PropTypes.object.isRequired
 };
 
-export default TextStats;
+export default withWidth()(TextStats);
